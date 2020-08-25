e38a39dfb1eb7a2ff0e5a716769c54af1cd05632
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-08-25 22:10:23 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.scss";
+import "./js/initialHomePage";
+import "./js/searchAndPlaginationHomePage";
+import "./js/navigation";
+import "./js/filmDetailsPage";
+import "./js/libraryPage";
