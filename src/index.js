b12a84e93699aacdcf12964e086a4c52d85e786c
Oksana <okoksana96@gmail.com>
2020-08-29 21:54:03 +0300

    oksana-com

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import './styles.scss';
-// import 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css';
+import apiServices from './js/apiServices';
 import './js/initialHomePage';
 import './js/searchAndPlaginationHomePage';
 import './js/navigation';
 import './js/filmDetailsPage';
 import './js/libraryPage';
-import apiServices from './js/apiServices';
+
 import detailsPage from './templates/detailsPage.hbs';
-
-const getFilms = async() => {
-    const films = await apiServices.get();
-    console.log(films[0]);
-    const markup = detailsPage(films[2]);
-
-    document.querySelector('.main').insertAdjacentHTML('beforeend', markup);
-};
-
-getFilms();
-
-document.querySelector('body').onclick = onBodyClick;
-
-function onBodyClick(event) {
-    // console.log(event);
-    if (event.target === document.querySelector('.detailsPage__btnAddFavorite')) {
-        console.log('addBtn');
-        event.target.textContent = 'qweqwe';
-    }
-}
