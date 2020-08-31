65f4d55ec1f58741bd216c62883ff008cad4dd63
RomanPidlisnychyi <myLuckyhelper@gmail.com>
2020-08-31 14:50:38 +0300

    delete src

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './styles.scss';
-import './js/initialHomePage';
-import './js/searchAndPlaginationHomePage';
-import './js/navigation';
-import './js/filmDetailsPage';
-import './js/libraryPage';
